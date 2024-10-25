2cddb02e8c055adc0d5d9b47588a8a64b112a32f
benrichard-amd <ben.richard@amd.com>
2024-10-26 02:08:25 +0530

    Fix typo
    Signed-off-by: benrichard-amd <ben.richard@amd.com>

--- src/omniperf_soc/soc_gfx908.py
+++ src/omniperf_soc/soc_gfx908.py
@@ -63,7 +63,7 @@ class gfx908_soc(OmniSoC_Base):
         self._mspec.lds_banks_per_cu = 32
         self._mspec.pipes_per_gpu = 4
         # --showmclkrange is broken in Mi100, hardcode freq
-        if self._mspec.gpu_model == "MI300":
+        if self._mspec.gpu_model == "MI100":
             if self._mspec.max_mclk == None or self._mspec.cur_mclk == None:
                 self._mspec.max_mclk = 1200
                 self._mspec.cur_mclk = 1200

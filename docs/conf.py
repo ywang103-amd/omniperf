bb388b03f110c11d47482cb23122de44776f6555
Peter Park <peter.park@amd.com>
2025-01-16 10:56:51 -0500

    update LICENSE year to 2025 (#530)

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -37,7 +37,7 @@ with open("../VERSION", encoding="utf-8") as f:
 # project info
 project = "ROCm Compute Profiler"
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved."
 version = version_number
 release = version_number
 

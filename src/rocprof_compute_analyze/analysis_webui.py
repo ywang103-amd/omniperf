f44e40998a3d5236f2cae02c262b6f89c3cb3eff
Yang Wang <ywang103@amd.com>
2025-02-14 12:43:01 -0600

    fix missing parameter of spatial multiplexing for create_df_pmc

--- src/rocprof_compute_analyze/analysis_webui.py
+++ src/rocprof_compute_analyze/analysis_webui.py
@@ -294,6 +294,7 @@ class webui_analysis(OmniAnalyze_Base):
             self._runs[self.dest_dir].raw_pmc = file_io.create_df_pmc(
                 self.dest_dir,
                 self.get_args().nodes,
+                self.get_args().spatial_multiplexing,
                 self.get_args().kernel_verbose,
                 args.verbose,
             )
